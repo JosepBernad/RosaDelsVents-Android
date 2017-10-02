46466c93c8348ba2325f4a96ad738b34cda8d56e
Sigmund Freud <josep.bernad@est.fib.upc.edu>
2017-10-02 10:44:36 +0200

    Added .idea

--- app/src/main/java/com/example/sigmundfreud/rosadelsvents/MainActivity.java
+++ app/src/main/java/com/example/sigmundfreud/rosadelsvents/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.sigmundfreud.rosadelsvents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.buttonLevel1:
+                Log.v("1 pressed","");
+                break;
+            case R.id.buttonLevel2:
+                Log.v("2 pressed","");
+                break;
+            case R.id.buttonLevel3:
+                Log.v("3 pressed","");
+                break;
+        }
+
+    }
 
 }
