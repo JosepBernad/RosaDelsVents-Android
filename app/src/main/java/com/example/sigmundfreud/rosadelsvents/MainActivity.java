b0df28cddd07d78aa1b2b6cbaf711c27893b4f4e
Sigmund Freud <josep.bernad@est.fib.upc.edu>
2017-10-03 18:30:22 +0200

    Working on Level 1

--- app/src/main/java/com/example/sigmundfreud/rosadelsvents/MainActivity.java
+++ app/src/main/java/com/example/sigmundfreud/rosadelsvents/MainActivity.java
@@ -1,35 +1,37 @@
 package com.example.sigmundfreud.rosadelsvents;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Rosa dels Vents");
     }
 
-    public void onClick(View view) {
 
-        switch (view.getId())
-        {
-            case R.id.buttonLevel1:
-                Log.v("1 pressed","");
-                break;
-            case R.id.buttonLevel2:
-                Log.v("2 pressed","");
-                break;
-            case R.id.buttonLevel3:
-                Log.v("3 pressed","");
-                break;
-        }
-
+    public void level1ButtonPressed(View view) {
+        Intent intent = new Intent(this, Level1Activity.class);
+        startActivity(intent);
     }
 
+    public void level2ButtonPressed(View view) {
+        Intent intent = new Intent(this, Level2Activity.class);
+        startActivity(intent);
+    }
+
+    public void level3ButtonPressed(View view) {
+        Intent intent = new Intent(this, Level3Activity.class);
+        startActivity(intent);
+    }
 }
